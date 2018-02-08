038acd073462cff84c656b0bd527108e43e441f3
Christian Tavares <chrisadrianotav@gmail.com>
2018-02-08 07:21:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 function theBeatlesPlay(musicians, instruments) {
-  var array = []
-  
+  var array = [];
+  for(let i = 0; i < 4; i++){
+    array.push(`${musicians[0]} plays ${instruments[0]}`);
+  }
+  return array
 }
 
 
 johnLennonFacts(facts){
+  while() {
+    facts[] + "!";
+  }
+  return facts;
 }
